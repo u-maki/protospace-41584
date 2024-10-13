453d8ca536c757496c5292625cce97d41d0d118e
u-maki <maki0825.w@gmail.com>
2024-10-13 19:13:28 +0900

    Prototypeモデルおよびテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_13_042305) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_13_092002) do
+  create_table "prototypes", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.text "profile", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_13_042305) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "prototypes", "users"
 end
