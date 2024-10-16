69f76629f2ff2b7f837b2bf1875c7fe7a7222a7d
u-maki <maki0825.w@gmail.com>
2024-10-16 09:42:09 +0900

    render機能追記

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,21 +1,34 @@
 class PrototypesController < ApplicationController
 
   def index
+    @prototypes = Prototype.all
   
   end
 
   def new
-    @prototype = prototype.new
+    @prototype = Prototype.new
     
   end
 
   def create
-    
+     @prototype = Prototype.new(prototype_params)
+       
+    if @prototype.save
+      redirect_to root_path
+      
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def prototype_params
-    params.require(:prototype).permit(:catch_copy, :title, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:catch_copy, :title, :image, :concept).merge(user_id: current_user.id)
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    
   end
 end
