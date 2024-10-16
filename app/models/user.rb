520b1635d14290040aa3e3a6270b1fc9ab944e33
u-maki <maki0825.w@gmail.com>
2024-10-16 17:18:10 +0900

    コメントデーブルの作成とアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweets
+  has_many :prototypes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
